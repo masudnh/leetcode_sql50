6db625f347d11f5e0a761fe4d1ac94225e1562c6
Mas'udin Nur Hidayat <97170369+masudnh@users.noreply.github.com>
2025-03-16 03:02:29 +0700

    Time: 1101 ms (44%), Space: 0B (100%) - LeetHub

--- 1877-find-followers-count/1877-find-followers-count.sql
+++ 1877-find-followers-count/1877-find-followers-count.sql
@@ -2,9 +2,6 @@
 SELECT
     user_id
     , COUNT(follower_id) AS followers_count
-FROM
-    Followers
-GROUP BY
-    user_id
-ORDER BY
-    user_id ASC
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC
